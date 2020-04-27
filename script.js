7916bf2121244c5b65e0edf88c674dad1fc38715
VaderSteve76 <alverson.s@yahoo.com>
2020-04-27 18:16:43 -0400

    Added function to close navbar on outside click

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
 
+// Close navbar when outside click
+function closeNavbar(e) {
+  if (
+    document.body.classList.contains('show-nav') &&
+    e.target !== toggle &&
+    !toggle.contains(e.target) &&
+    e.target !== navbar &&
+    !navbar.contains(e.target)
+  ) {
+    document.body.classList.toggle('show-nav');
+    document.body.removeEventListener('click', closeNavbar);
+  } else if (!document.body.classList.contains('show-nav')) {
+    document.body.removeEventListener('click', closeNavbar);
+  }
+};
+
 
 // Event Listeners
 // Toggle navbar
