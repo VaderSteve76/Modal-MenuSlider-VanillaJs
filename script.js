c37dcdd47de9c31e797d0a5dbffe7eba989ad32b
VaderSteve76 <alverson.s@yahoo.com>
2020-04-27 18:02:34 -0400

    Added EventListeners

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
+
+
+// Event Listeners
+// Toggle navbar
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+  document.body.addEventListener('click', closeNavbar);
+});
+
+// Show modal
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
+
+// Hide modal 
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
+
+// Hide modal on outside click
+window.addEventListener('click', e => {
+  e.target == modal ? modal.classList.remove('show-modal') : false;
+});
